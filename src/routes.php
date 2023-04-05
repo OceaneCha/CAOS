d966a1c7a75ec50bfd202a55a21668154988da34
Sandra331 <[sandraguillemeton@gmail.com](mailto:sandraguillemeton@gmail.com)>
2023-04-05 16:50:47 +0200

    created theme controller

--- src/routes.php
+++ src/routes.php
@@ -7,10 +7,11 @@
 //          3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
+    'themes' => ['HomeController', 'index',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
+    '' => ['ThemeController', 'index',],
 ];
