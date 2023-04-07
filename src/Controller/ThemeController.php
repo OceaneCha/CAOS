78c7e0ed5cbeecab0e54e5a94519678d79b77749
CaroleGG <[caroguerite@hotmail.com](mailto:caroguerite@hotmail.com)>
2023-04-07 11:12:20 +0200

    recup en echo uniquement

--- src/Controller/ThemeController.php
+++ src/Controller/ThemeController.php
@@ -16,4 +16,15 @@ class ThemeController extends AbstractController
 
         return $this->twig->render('Theme/index.html.twig', ['themes' => $themes]);
     }
+
+       /**
+     * Show informations for a specific item
+     */
+    public function show(int $id): string
+    {
+        $themeManager = new ThemeManager();
+        $theme = $themeManager->selectOneById($id);
+
+        return $this->twig->render('Quiz/index.html.twig', ['theme' => $theme]);
+    }
 }
