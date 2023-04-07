0bc856b5df41b4f45c2337ca61cf8d1b2a1ee261
CaroleGG <[caroguerite@hotmail.com](mailto:caroguerite@hotmail.com)>
2023-04-07 11:12:20 +0200

    recup en echo uniquement

--- src/Model/QuestionManager.php
+++ src/Model/QuestionManager.php
@@ -3,12 +3,43 @@
 namespace App\Model;
 
 use App\Model\AnswerManager;
+use PDO;
 
 class QuestionManager extends AbstractManager
 {
     public const TABLE = 'question';
 
     private int $id = 0;
+
+    public function showQuestions(int $id)
+    {
+        $query = "SELECT q.id as qid ,q.title ,t.id as tid FROM question q RIGHT JOIN theme t ON t.id = q.theme_id WHERE q.theme_id =$id";
+        $statement = $this->pdo->query($query);
+       // $statement ->execute();
+        
+       
+        $questions= $statement->fetchAll(PDO::FETCH_OBJ);
+        //var_dump($questions);
+
+        foreach($questions as $question) {
+            echo "<div>$question->title.' '.$question->qid </div>";
+           
+            $queryAnswer = "SELECT * FROM answer WHERE question_id = $question->qid";
+            $statement = $this->pdo->query($queryAnswer);
+            // $statement ->execute();
+             
+            
+             $answers= $statement->fetchAll(PDO::FETCH_OBJ);
+             foreach($answers as $answer){
+                echo "<div>$answer->title.' '.$answer->id </div>";
+             }
+
+             echo"<hr>";
+
+        }
+     // return $questions;
+        
+    }
 
     // fetch the right Answer instance
     public function getCorrectAnswer()
@@ -18,4 +49,8 @@ class QuestionManager extends AbstractManager
         $stmt->execute();
         return $stmt->fetch();
     }
+
+   
+
+
 }
--- src/routes.php
+++ src/routes.php
@@ -14,4 +14,6 @@ return [
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
     '' => ['ThemeController', 'index',],
+    'themes' => ['ThemeController', 'show',['id'] ],
+    'quiz' => ['QuizController', 'index'],
 ];
