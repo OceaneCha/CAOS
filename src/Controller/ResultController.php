8ac3229560d753c496decc5bc781cd5d1b3dcf13
Angeline90 <angeline.gauron@gmail.com>
2023-04-14 18:17:53 +0200

    result in progress

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -1,36 +1,46 @@
-<?php 
+<?php
 
 namespace App\Controller;
 
+use App\Model\QuestionManager;
 use App\Model\ResultManager;
+use App\Model\ThemeManager;
 
 class ResultController extends ThemeController
 {
 
     public function result()
-    {   
-       // session_start();
+    {
         $reponses = $_POST;
         $score = 0;
-        // var_dump($_POST);
-    
+
         // Récupération des réponses correctes
         $resultManager = new ResultManager();
         $correctAnswers = $resultManager->getCorrectAnswers();
+
+        // Récupération des questions, réponses et du thème
+        //à faire => rendre le tout dynamique
+        $themeManager = new ThemeManager();
+        $theme = $themeManager->selectOneById(1);
+        $questionManager = new QuestionManager();
+        $questions = $questionManager->showQuestions(1);
 
         // Comparaison des réponses données avec les bonnes réponses
         foreach ($correctAnswers as $correctAnswer) {
             $questionId = $correctAnswer['question_id'];
             $answerId = $correctAnswer['answer_id'];
-            if (isset($reponses[$questionId]) && $reponses[$questionId] == $answerId) {
+            if (isset($reponses[$questionId]) && $reponses[$questionId] == $answerId) 
+            {
                 $score++;
             }
-        }         
+        }
 
         return $this->twig->render('Quiz/result.html.twig', [
             'score' => $score,
-            'questions' => $reponses,
+            'questions' => $questions,
+            'reponses' => $reponses,
             'correctAnswers' => $correctAnswers,
+            'theme' => $theme,
         ]);
     }
 }
