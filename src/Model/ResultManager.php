bb93fc937ab4ffc71fb0a8335c6e851b81ff4c0c
Angeline90 <angeline.gauron@gmail.com>
2023-04-25 09:49:03 +0200

    merge

--- src/Model/ResultManager.php
+++ src/Model/ResultManager.php
@@ -10,11 +10,10 @@ class ResultManager extends AbstractManager
 
     public function getCorrectAnswers()
     {
-        $result = "
-        SELECT q.id AS question_id, a.id AS answer_id 
-            FROM answer a JOIN question q 
-        ON a.question_id = q.id 
-            WHERE a.isCorrect = 1";
+        $result = "SELECT q.id AS question_id, a.id AS answer_id 
+                   FROM answer a 
+                   JOIN question q ON a.question_id = q.id 
+                   WHERE a.isCorrect = 1";
         $statement = $this->pdo->prepare($result);
         $statement->execute();
         $result = $statement->fetchAll(PDO::FETCH_ASSOC);
